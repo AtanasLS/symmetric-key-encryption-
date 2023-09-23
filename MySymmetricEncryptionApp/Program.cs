89d4db9554abd578f4a528724d032754d7d80c18
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-09-23 05:15:16 +0200

    adding simple cli app and setting the dotnet on linux..

---
+++ MySymmetricEncryptionApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MySymmetricEncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My CLI Application!");
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine($"Hello, {name}! Nice to meet you.");
+            }
+            else
+            {
+                Console.WriteLine("Hello, anonymous! Nice to meet you.");
+            }
+        }
+    }
+}
+
